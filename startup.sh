af0f592f5fec116aacb4300fd51019566a034853
kamchatka-school <tologinprojectschool@gmail.com>
2026-09-22 19:00:07 +0300

    Improve Docker deployment configuration

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn main:app --bind 0.0.0.0:8000 --worker-class uvicorn.workers.UvicornWorker
+exec gunicorn main:app --bind 0.0.0.0:8000 --worker-class uvicorn.workers.UvicornWorker --workers 1
